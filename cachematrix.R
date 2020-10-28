cd78901c88e5c738111a6ccdd9906d594fb841a5
Dutch-Steve <72150233+Dutch-Steve@users.noreply.github.com>
2020-10-28 15:53:05 +0100

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+setwd('C:/Users/Steven/Documents/R.Studio/Bestanden - RStudio/Programming.R_ assingment2_LexicalScoping')
 
-## Write a short comment describing this function
+## Just toke a sample from the matrix named 'x'
+## then set the solved value 'i' as null.
+## After that, the value is changed 'mean' to 'inverse'
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix(sample(1:250),5,5)) {
+  i <- NULL
+  set <- function(y){
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) i <<- inverse
+  getinv <- function() i
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Changed 'mean' to 'inverse' and 'm' to 'i'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinv()
+  if(!is.null(i)){
+    message("getting inverse data")
+    return(i)
+  }
+  data <- x$get()
+  i <- inverse(data, ...)
+  x$setinv(i)
+  i
 }
